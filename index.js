541f47b6c27b221f6c6a6c535038526a823c8436
John Brown <johnbrownsinbox@gmail.com>
2022-02-22 19:33:43 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=2;
+const num2=31;
+const num3=11;
+const num4=7;
+const multiply = num1*num2;
 
-// Write your code here
+const random=Math.ceil(Math.random());
+
+const mod= num3%num4;
+
+const max = Math.max(3,1,12,20,2);
